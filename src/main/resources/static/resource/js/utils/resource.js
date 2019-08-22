efcf4e42a0b07725726c8bd3f5c80314484fe90e
antwigambrah <gambrahantwi23@gmail.com>
2019-08-22 13:29:30 +0000

    update ui

--- src/main/resources/static/resource/js/utils/resource.js
+++ src/main/resources/static/resource/js/utils/resource.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.baseURL="http://localhost:8080/api/";
+axios.defaults.baseURL="/api/";
 
 
 /**
